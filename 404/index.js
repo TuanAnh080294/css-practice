191f0ca95b3b1c4109626f3d311922f631490394
TuanAnh080294 <tuananh080294@gmail.com>
2019-07-18 22:28:01 +0700

    404-1

---
+++ 404/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var body = document.body;
+	var arrayColor = ["#FF6600", "#FF0000", "#880000", "#FF9933", "#ff3300", "#FF3366"];
+
+	setInterval(createStar, 50);
+
+	function createStar() {
+		var right = Math.floor(Math.random() * 500);
+		var top = Math.floor(Math.random() * screen.height);
+		var star = document.createElement("div");
+		star.classList.add("star");
+		body.appendChild(star);
+
+		setInterval(runStar, 10);
+		star.style.top = top + "px";
+		star.style.background = arrayColor[Math.floor(Math.random() * 5)];
+		function runStar() {
+			if(right >= screen.width) {
+				star.remove();
+			}
+			right += 3;
+			star.style.right =  right + "px";
+		}
+	}
+	
+});
+
