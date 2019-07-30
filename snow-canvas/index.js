5993e28347e3b20d80e3647178116c83ab69963e
TuanAnh080294 <tuananh080294@gmail.com>
2019-07-30 20:21:03 +0700

    snow-canvas-2

--- snow-canvas/index.js
+++ snow-canvas/index.js
@@ -11,26 +11,51 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     function Snow() {
-        this.x = 10;
-        this.y = 10;
-        this.radius = 10;
-        this.color = "#FFF";   
+        this.radius = Math.random() * 5;
+        this.x = Math.random() * canvas.width;
+        this.y = -this.radius;
+        this.color = "#FFF";
+        this.velocity = {
+            x: Math.random() * 4 - 2,
+            y: Math.random() * 3 + 2
+        }; 
     }
 
-    Snow.prototype.draw = function() {
+    Snow.prototype.draw = function(brush) {
         brush.beginPath();
-        brush.arc(this.x, this.y, radius, 0, 2 * Math.PI);
-        brush.fillStyle(this.color);
+        brush.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        brush.fillStyle = this.color;
+        // brush.shadowColor = this.color;
+        // brush.shadowBlur = 1;
         brush.fill();
         brush.closePath();
     }
 
+    Snow.prototype.update = function() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        if(this.y >= canvas.height) {
+            this.velocity.y = 0;
+        }
+        this.draw(brush);
+    }
+
+    var arraySnow = [];
     function init() {
-        
+        arraySnow.push(new Snow());
     }
 
-    function update() {
-
+    function animate() {
+        window.requestAnimationFrame(animate);
+        brush.clearRect(0, 0, canvas.width, canvas.height);
+        arraySnow.forEach(function(snow) {
+            snow.update();
+         });
+        if(arraySnow.length > 1000) {
+            arraySnow.splice(0, 1);
+        }
+        init();
     }
 
+    animate();
 });
